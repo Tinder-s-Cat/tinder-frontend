85dcb04aa47e0160822d94cbf5d8abf817e02772
abdulmajid <abdulmajid08180881@gmail.com>
2021-05-30 22:36:58 +0700

    added skipButton

--- client/src/components/SwapCard.jsx
+++ client/src/components/SwapCard.jsx
@@ -1,27 +1,37 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Cards } from '../components/Cards'
+import { skipCard } from '../store/actions/action'
 
 export default function SwapCard() {
 	const cats = useSelector(state => state.randomCards)
-	console.log(cats);
+	// console.log(cats);
 	const dispatch = useDispatch()
 
 	// useEffect(() => {
 		
 	// }, []);
 
+	const handleSkip = () => {
+		dispatch(skipCard())
+	}
+
 	return (
 		<>
 			<div className="p-56">
 				<div className="w-96 m-auto ">
-					{
+					{/* {
 						cats.map((cat, idx) => <Cards key={idx} cat={cat} />)
+					} */}
+					
+					{
+						cats.length === 0 ? <div>DATA NOT FOUND</div> :
+						<Cards cat={cats[0]} />
 					}
 				</div>
 			</div>
 
-			<button className="bg-blue hover:bg-blue-light text-black font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded">
+			<button onClick={() => handleSkip()} className="bg-blue hover:bg-blue-light text-black font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded">
 				Skip
 			</button>
 			<button className="bg-blue hover:bg-blue-light text-black font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded">
--- client/src/store/reducers/reducer.js
+++ client/src/store/reducers/reducer.js
@@ -26,6 +26,29 @@ const initialState = {
 				"updatedAt": "2021-05-29T06:08:35.987Z"
 			}
 		},
+		{
+			"id": 4,
+			"UserId": 2,
+			"name": "Pikachu",
+			"gender": "female",
+			"age": 2,
+			"race": "sphynx",
+			"status": true,
+			"profilePicture": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/American_Shorthair.jpg/1200px-American_Shorthair.jpg",
+			"description": "ayo kenalan!!",
+			"createdAt": "2021-05-29T09:13:22.324Z",
+			"updatedAt": "2021-05-29T09:21:24.122Z",
+			"User": {
+				"id": 2,
+				"username": "test3",
+				"location": "jakarta barat",
+				"email": "test3@mail.com",
+				"password": "$2b$10$DtDM3H1OKMLrKsj1iwcYFuvFzTOZgldr57VfJiOLRqWcWKUVeeuwy",
+				"profilePicture": "https://c.files.bbci.co.uk/12A9B/production/_111434467_gettyimages-1143489763.jpg",
+				"createdAt": "2021-05-29T09:11:52.415Z",
+				"updatedAt": "2021-05-29T09:11:52.415Z"
+			}
+		}
 	],
 	profile: {},
 	loading: false,
