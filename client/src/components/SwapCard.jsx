03d430f34048f8a07a74306587249c490408eda6
abdulmajid <abdulmajid08180881@gmail.com>
2021-05-30 22:45:39 +0700

    added styling button

--- client/src/components/SwapCard.jsx
+++ client/src/components/SwapCard.jsx
@@ -18,28 +18,32 @@ export default function SwapCard() {
 
 	return (
 		<>
-			<div className="p-56">
-				<div className="w-96 m-auto ">
-					{/* {
-						cats.map((cat, idx) => <Cards key={idx} cat={cat} />)
-					} */}
-					
-					{
-						cats.length === 0 ? <div>DATA NOT FOUND</div> :
-						<Cards cat={cats[0]} />
-					}
+			<div className="flex flex-col">
+				<div className="p-16">
+					<div className="w-96 m-auto ">
+						{/* {
+							cats.map((cat, idx) => <Cards key={idx} cat={cat} />)
+						} */}
+
+						{
+							cats.length === 0 ? <div>DATA NOT FOUND</div> :
+							<Cards cat={cats[0]} />
+						}
+					</div>
+				</div>
+				
+				<div className="space-x-2">
+					<button onClick={() => handleSkip()} className="bg-blue-500 text-black font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded">
+						Skip
+					</button>
+					<button className="bg-blue-500  text-black font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded">
+						Refresh
+					</button>
+					<button className="bg-blue-500  text-black font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded">
+						Like
+					</button>
 				</div>
 			</div>
-
-			<button onClick={() => handleSkip()} className="bg-blue hover:bg-blue-light text-black font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded">
-				Skip
-			</button>
-			<button className="bg-blue hover:bg-blue-light text-black font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded">
-				Refresh
-			</button>
-			<button className="bg-blue hover:bg-blue-light text-black font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded">
-				Like
-			</button>
 		</>
 	)
 }
