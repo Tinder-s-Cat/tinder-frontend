ceadec7e48f6d38e560040ba7f90e0c812ba1e57
diazagfanuribi <diazagfa@gmail.com>
2021-06-01 15:29:46 +0700

    edit form

--- client/src/components/EditForm.jsx
+++ client/src/components/EditForm.jsx
@@ -26,22 +26,7 @@ export default function EditForm({ setShowModal, payload }) {
 			description: cat.description,
 		}
 		dispatch(editCat({ payload: data, userId, id: payload.id }))
-		const Toast = Swal.mixin({
-			toast: true,
-			position: 'top-end',
-			showConfirmButton: false,
-			timer: 3000,
-			timerProgressBar: true,
-			didOpen: (toast) => {
-				toast.addEventListener('mouseenter', Swal.stopTimer)
-				toast.addEventListener('mouseleave', Swal.resumeTimer)
-			},
-		})
 
-		Toast.fire({
-			icon: 'success',
-			title: 'Edit success!',
-		})
 		setShowModal()
 	}
 	function addName(event) {
