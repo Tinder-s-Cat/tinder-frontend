6e35a0a80802191a0d6c6a18edfcd9c83a254dbd
diazagfanuribi <diazagfa@gmail.com>
2021-06-02 21:50:59 +0700

    add font style

--- client/src/screen/NotFound.jsx
+++ client/src/screen/NotFound.jsx
@@ -4,7 +4,7 @@ export default function NotFound() {
 	return (
 		<div className="h-screen w-screen bg-yellow-400 flex flex-col justify-center items-center">
 			<img src={Logo} className="w-1/6 animate-pulse" alt="" />
-			<h1 className="bg-gray-600 text-white text-xl rounded-lg p-1">
+			<h1 className="bg-gray-600 text-white text-xl mt-2 rounded-lg p-1">
 				404 Not Found
 			</h1>
 		</div>
